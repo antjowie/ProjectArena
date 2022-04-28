e0d5fb7de3c45338d66ca4e347bd4873d284cbb3
Angelo Anthony Rettob <7332321+antjowie@users.noreply.github.com>
2022-04-29 00:21:53 +0200

    Added menu item to build and launch Web target

---
+++ Assets/Scripts/Editor/BuildScripts.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using Debug = UnityEngine.Debug;
+
+namespace Assets.Scripts.Editor
+{
+    internal class BuildScripts
+    {
+        [MenuItem("Build/Web")]
+        static void BuildWeb()
+        {
+            BuildPlayerOptions options = new BuildPlayerOptions();
+            options.scenes = new string[] { "Assets/Scenes/TestScene.unity" };
+            options.locationPathName = "Builds/Web";
+            options.target = BuildTarget.WebGL;
+            options.options = BuildOptions.None;
+
+            Debug.Log("Building for web target...");
+            BuildReport report = BuildPipeline.BuildPlayer(options);
+            BuildSummary summary = report.summary;
+
+            if (summary.result == BuildResult.Succeeded)
+            {
+                Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+            }
+
+            if (summary.result == BuildResult.Failed)
+            {
+                Debug.Log("Build failed");
+            }
+        }
+
+        [MenuItem("Build/Host server")]
+        static void HostServer()
+        {
+            try
+            {
+                using (Process process = new Process())
+                {
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.FileName = Path.Combine("Scripts", "SimpleWebServer.exe");
+                    process.StartInfo.Arguments = new string("Builds/Web 8080");
+                    process.StartInfo.WorkingDirectory = Directory.GetCurrentDirectory();
+
+                    Debug.Log("Server started, access via http://localhost:8080");
+                    process.Start();
+
+                    Process.Start("chrome.exe", "http://localhost:8080");
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e.Message);
+            }
+        }
+
+    }
+}

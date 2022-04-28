8510e08d06a427f8762590ec0356415cce5ee654
Angelo Anthony Rettob <7332321+antjowie@users.noreply.github.com>
2022-04-28 23:07:34 +0200

    Added basic player scene

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,23 +1,17 @@
+using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovement : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        var Movement = new float3();
-        Movement.x = Input.GetAxisRaw
+        if (!isLocalPlayer) return;
 
-        ("Horizontal");
+        var Movement = new float3();
+        Movement.x = Input.GetAxisRaw("Horizontal");
         Movement.y = Input.GetAxisRaw("Vertical");
 
         transform.Translate(Movement * 10f * Time.deltaTime);
